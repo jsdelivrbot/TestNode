47ba843b9e8df7bd411777e5cdb64b3351147cfb
Ahmet Çümen <ahmet.cumen@digitus.com.tr>
2018-07-05 09:38:08 +0300

    Toplama işlemi yapan servise yazıldı.

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.post('/topla', function(req,res){
+  var value = parseInt(req.query.value1) + parseInt(req.query.value2);
+  var bodySend = {}
+  if(value){
+       bodySend = {
+        "status": 200,
+        "data": {
+          "işlem" : "Toplama",
+          "Değer 1": parseInt(req.query.value1),
+          "Değer 2": parseInt(req.query.value2),
+          "sonuc": value
+        }
+      }
+  }
+  else{
+     bodySend = {
+      "status": 400,
+      "error": "Bad request"
+    }
+  }
+  res.status(200).send(bodySend)
+
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
